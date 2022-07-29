a8be1e8dd111fb73e0662816dbeabb62d3f8a458
BaczkiewiczDawid <baczkiewicz.dawid22@gmail.com>
2022-07-29 13:01:08 +0200

    getting top 10 selling cars from db

--- server.js
+++ server.js
@@ -1,17 +1,34 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const mysql = require('mysql');
-require('dotenv').config();
+const mysql = require("mysql");
+const cors = require("cors");
+require("dotenv").config();
+
+app.use(cors());
 
 const db = mysql.createConnection({
-    host: process.env.HOST,
-    user: process.env.USER,
-    password: process.env.PASSWORD,
-    database: process.env.DB
-})
+  host: process.env.HOST,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  database: process.env.DB,
+});
 
 db.connect();
 
-console.log('Server running')
+app.get("/top-selling-models", (req, res) => {
+  const getTopSellingModels =
+    "SELECT gerna_cars.manufactuer, gerna_cars.model, gerna_cars.price, count(gerna_cars.id) as sales FROM gerna_sales, gerna_cars WHERE gerna_sales.model = gerna_cars.id GROUP BY gerna_cars.id LIMIT 10";
 
-app.listen(3001)
+  db.query(getTopSellingModels, (err, result) => {
+    if (err) {
+      throw err;
+    } else {
+      console.log(result);
+      res.send(result);
+    }
+  });
+});
+
+console.log("Server running");
+
+app.listen(3001);
