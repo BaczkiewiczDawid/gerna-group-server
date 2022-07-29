f03ede2732b5d538cba18770a2f579145d012de7
BaczkiewiczDawid <baczkiewicz.dawid22@gmail.com>
2022-07-29 12:22:07 +0200

    mysql setup

--- server.js
+++ server.js
@@ -1,5 +1,16 @@
 const express = require('express');
 const app = express();
+const mysql = require('mysql');
+require('dotenv').config();
+
+const db = mysql.createConnection({
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DB
+})
+
+db.connect();
 
 console.log('Server running')
 
