5fd73206b6186bcb2ac4ab0503815a6620d4e24d
BaczkiewiczDawid <baczkiewicz.dawid22@gmail.com>
2022-08-01 20:40:35 +0200

    server disconnection error fix

--- server.js
+++ server.js
@@ -9,14 +9,46 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-const db = mysql.createConnection({
+// const db = mysql.createConnection({
+//   host: process.env.HOST,
+//   user: process.env.USER,
+//   password: process.env.PASSWORD,
+//   database: process.env.DB,
+// });
+
+// db.connect();
+
+const db_config = {
   host: process.env.HOST,
   user: process.env.USER,
   password: process.env.PASSWORD,
   database: process.env.DB,
-});
+}
 
-db.connect();
+let db;
+
+function handleDisconnect() {
+  db = mysql.createConnection(db_config); // Recreate the connection, since
+                                                  // the old one cannot be reused.
+
+  db.connect(function(err) {              // The server is either down
+    if(err) {                                     // or restarting (takes a while sometimes).
+      console.log('error when connecting to db:', err);
+      setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+    }                                     // to avoid a hot loop, and to allow our node script to
+  });                                     // process asynchronous requests in the meantime.
+                                          // If you're also serving http, display a 503 error.
+  db.on('error', function(err) {
+    console.log('db error', err);
+    if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+      handleDisconnect();                         // lost due to either server restart, or a
+    } else {                                      // connnection idle timeout (the wait_timeout
+      throw err;                                  // server variable configures this)
+    }
+  });
+}
+
+handleDisconnect();
 
 app.get("/top-selling-models", (req, res) => {
   const getTopSellingModels =
@@ -58,8 +90,6 @@ app.get("/employees-list", (req, res) => {
 
 app.post("/employee-details", (req, res) => {
   const selectedUser = req.body.selectedUser;
-
-  console.log(selectedUser);
 
   const getEmployeeDetails = `SELECT * FROM gerna_employees WHERE id = ${selectedUser}`;
 
