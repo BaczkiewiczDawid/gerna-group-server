dd2bbf3c9da281f166211166de86dc2549cc92fb
BaczkiewiczDawid <baczkiewicz.dawid22@gmail.com>
2022-08-22 13:55:27 +0200

    heroku

--- server.js
+++ server.js
@@ -17,6 +17,7 @@ const db_config = {
   database: process.env.DB,
 };
 
+
 let db;
 
 function handleDisconnect() {
@@ -41,6 +42,27 @@ handleDisconnect();
 setInterval(function () {
   db.query("SELECT 1");
 }, 5000);
+
+const Sequelize = require('sequelize');
+require('dotenv').config();
+
+let sequelize;
+
+// when deployed on Heroku
+if (process.env.JAWSDB_URL) {
+    sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+    // localhost
+    sequelize = new Sequelize(process.env.DB_NAME, 
+    process.env.DB_USER, 
+    process.env.DB_PASSWORD, {
+        host: 'localhost',
+        dialect: 'mysql',
+        port: 3306
+    });
+}
+
+module.exports = sequelize;
 
 app.get('/', (req, res) => res.send('Hello world'))
 
