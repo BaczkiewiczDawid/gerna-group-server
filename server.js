20c71b0a3eb605bfa1fe919deb33e84194e63123
BaczkiewiczDawid <baczkiewicz.dawid22@gmail.com>
2022-08-22 12:54:30 +0200

    heroku

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ const bodyParser = require("body-parser");
 const bcrypt = require("bcrypt");
 
 app.use(cors());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const db_config = {
   host: process.env.HOST,
@@ -149,7 +149,6 @@ app.post("/new-employee", (req, res) => {
       console.log(err);
     } else {
       res.send(randomPassword);
-      // res.send(result);
     }
   });
 });
