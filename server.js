585baa02df7c190db1187436799e1ab715513486
BaczkiewiczDawid <baczkiewicz.dawid22@gmail.com>
2022-08-27 10:53:04 +0200

    added cors config

--- server.js
+++ server.js
@@ -10,15 +10,15 @@ app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const corsOptions ={
-    origin: process.env.PORT | port, 
-    credentials:true,         
-    optionSuccessStatus:200
-}
+const corsOptions = {
+  origin: process.env.PORT | port,
+  credentials: true,
+  optionSuccessStatus: 200,
+};
 app.use(cors(corsOptions));
 
 const db_config = {
-  host: process.env.HOST,
+  host: 'https://gernagroup-server.herokuapp.com/',
   user: process.env.USER,
   password: process.env.PASSWORD,
   database: process.env.DB,
