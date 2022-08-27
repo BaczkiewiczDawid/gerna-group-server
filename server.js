fe6a4f3e5b1bd9e25b9e817f3bb824f97340026f
BaczkiewiczDawid <baczkiewicz.dawid22@gmail.com>
2022-08-27 10:49:34 +0200

    added cors config

--- server.js
+++ server.js
@@ -10,6 +10,13 @@ app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const corsOptions ={
+    origin: process.env.PORT | port, 
+    credentials:true,         
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
+
 const db_config = {
   host: process.env.HOST,
   user: process.env.USER,
