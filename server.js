fbcc40dcdfd3dec7b8fc19a12a7e455ded11a7e5
BaczkiewiczDawid <baczkiewicz.dawid22@gmail.com>
2022-08-27 11:14:34 +0200

    fix

--- server.js
+++ server.js
@@ -10,8 +10,6 @@ app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(cors);
-
 const db_config = {
   host: process.env.HOST,
   user: process.env.USER,
